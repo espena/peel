c74faa7576441f2f206926a36571bc492ca2cd28
Espen Andersen <espen.andersen@nrk.no>
2015-12-08 15:07:45 +0100

    Creates config directories if non-existent.

--- lib/configuration_file.inc.php
+++ lib/configuration_file.inc.php
@@ -2,11 +2,22 @@
   require_once( DIR_LIB . '/factory.inc.php' );
   class ConfigurationFile {
     private static $mConfigData;
+    private static function createDirectories() {
+      foreach( self::$mConfigData[ 'peeler_conf' ] as $dirName ) {
+        $dirName = self::$mConfigData[ 'config_directory' ] . '/' . $dirName;
+        if( !file_exists( $dirName ) ) {
+          mkdir( $dirName, 0755 );
+        }
+      }
+    }
     public static function parse() {
       if( !self::$mConfigData ) {
         $params = Factory::getParameters();
         $file = isset( $params[ 'conf' ] ) ? $params[ 'conf' ] : DIR_CONF . '/peel.conf';
         self::$mConfigData = parse_ini_file( $file, true, INI_SCANNER_NORMAL );
+        self::$mConfigData[ 'config_file_path' ] = $file;
+        self::$mConfigData[ 'config_directory' ] = dirname( $file );
+        self::createDirectories();
       }
       return self::$mConfigData;
     }
