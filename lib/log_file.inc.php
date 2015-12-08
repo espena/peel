04205ec464c73775ff90acde37b26a950929d191
Espen Andersen <espen.andersen@nrk.no>
2015-12-08 16:10:46 +0100

    Implementing logfile details.

--- lib/log_file.inc.php
+++ lib/log_file.inc.php
@@ -1,5 +1,22 @@
 <?php
-  class LogFile {
-    
+  require_once( DIR_LIB . '/factory.inc.php' );
+  require_once( DIR_LIB . '/i_logger.inc.php' );
+  class LogFile implements ILogger {
+    private function getPrefix( $level ) {
+      $now = date("Y-m-d H:i:s");
+      return sprintf( "%s - %s:", $now, str_pad( $level, 10, ' ', STR_PAD_RIGHT ) );
+    }
+    public function message( $str ) {
+      
+    }
+    public function warning( $str ) {
+      
+    }
+    public function error( $str ) {
+      
+    }
+    private function write( $str ) {
+
+    }
   }
 ?>
