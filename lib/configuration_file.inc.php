64896b3aac15d08dfaf11ff69a5de9feb3283ade
Espen Andersen <espen.andersen@nrk.no>
2015-12-08 15:32:15 +0100

    Implement parsing of enabled peelers.

--- lib/configuration_file.inc.php
+++ lib/configuration_file.inc.php
@@ -18,8 +18,21 @@
         self::$mConfigData[ 'config_file_path' ] = $file;
         self::$mConfigData[ 'config_directory' ] = dirname( $file );
         self::createDirectories();
+        self::parseEnabled();
       }
       return self::$mConfigData;
+    }
+    private static function parseEnabled() {
+      self::$mConfigData[ 'peelers' ] = array();
+      $cEnabledDir = sprintf( '%s/%s', self::$mConfigData[ 'config_directory' ], self::$mConfigData[ 'peeler_conf' ][ 'dir_enabled' ] );
+      $filePattern = sprintf( '%s/*.conf', $cEnabledDir );
+      $confEnabled = glob( $filePattern );
+      foreach( $confEnabled as $confFile ) {
+        $c = parse_ini_file( $confFile, true, INI_SCANNER_NORMAL );
+        $c[ 'config_file_path' ] = $confFile;
+        array_push( self::$mConfigData, $c );
+      }
+      print_r( self::$mConfigData );
     }
   }
 ?>
