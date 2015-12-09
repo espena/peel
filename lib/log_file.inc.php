bc5e972beee3f5ff7042f83ee0fd0432e75acabf
Espen Andersen <espen.andersen@nrk.no>
2015-12-09 19:29:04 +0100

    Implementing file write operations.

--- lib/log_file.inc.php
+++ lib/log_file.inc.php
@@ -13,16 +13,16 @@
       return sprintf( "%s - %s:", $now, str_pad( $level, 10, ' ', STR_PAD_RIGHT ) );
     }
     public function message( $str ) {
-      
+      $this->write( $this->getPrefix( 'MESSAGE' ), $str );
     }
     public function warning( $str ) {
-      
+      $this->write( $this->getPrefix( 'WARNING' ), $str );
     }
     public function error( $str ) {
-      
+      $this->write( $this->getPrefix( 'ERROR' ), $str );
     }
-    private function write( $str ) {
-
+    private function write( $prefix, $str ) {
+      file_put_contents( $this->mLogFile, sprintf( "%s %s\n", $prefix, $str ) );
     }
   }
 ?>
