37b31d4c47d5213f958be133f38e518906c3093d
Espen Andersen <espen.andersen@nrk.no>
2015-12-09 19:20:05 +0100

    Resolv log file name (default or config specified).

--- lib/log_file.inc.php
+++ lib/log_file.inc.php
@@ -2,6 +2,12 @@
   require_once( DIR_LIB . '/factory.inc.php' );
   require_once( DIR_LIB . '/i_logger.inc.php' );
   class LogFile implements ILogger {
+    public function __construct() {
+      $c = Factory::getConfig();
+      $this->mLogFile = isset( $c[ 'logging' ][ 'error_log' ] )
+                      ? $c[ 'logging' ][ 'error_log' ]
+                      : '/var/log/peeler.log';
+    }
     private function getPrefix( $level ) {
       $now = date("Y-m-d H:i:s");
       return sprintf( "%s - %s:", $now, str_pad( $level, 10, ' ', STR_PAD_RIGHT ) );
