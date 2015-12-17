59b77b2bee22fec51bc89c49b64f27ebbb9462ad
Espen Andersen <espen.andersen@nrk.no>
2015-12-17 01:54:24 +0100

    Adding documentation blocks.

--- lib/app_peel_engine.inc.php
+++ lib/app_peel_engine.inc.php
@@ -1,16 +1,83 @@
 <?php
+ /**
+  * PEEL Document Downloader engine.
+  *
+  * Peel Engine class.
+  *
+  * PHP version > 5.5
+  *
+  * @category   peel
+  * @package    PEEL
+  * @author     Espen Andersen <post@espenandersen.no>
+  * @copyright  2016 The Author
+  * @license    GNU General Public License, version 3
+  * @link       https://github.com/espena/peel
+  */
+
   require_once( DIR_LIB . '/i_application.inc.php' );
   require_once( DIR_LIB . '/factory.inc.php' );
+
+ /**
+  * Peel engine class.
+  *
+  * The class that does the actual job. Initializes and runs individual
+  * peelers in sequence.
+  *
+  * /IApplication classes are instantiated in /Factory.
+  *
+  * @category   peel
+  * @package    PEEL
+  * @author     Espen Andersen <post@espenandersen.no>
+  * @copyright  2016 The Author
+  * @license    GNU General Public License, version 3
+  * @link       https://github.com/espena/peel
+  */
   class AppPeelEngine implements IApplication {
+
+   /**
+    * The decorated (base) application instance.
+    *
+    * @var IApplication $mBase
+    */
     private $mBase;
+
+   /**
+    * Array of enabled peelers.
+    *
+    * @var IPeeler[] $mPeelers
+    */
     private $mPeelers;
+
+   /**
+    * Constructor.
+    *
+    * @param IApplication $base The decorated (base) application instance.
+    * @return void
+    */
     public function __construct( $base ) {
       $this->mBase = $base;
       $this->mPeelers = array();
     }
+
+   /**
+    * Get configuration array.
+    *
+    * Returns the application configuration, including the settings for
+    * individually enabled peelers.
+    *
+    * @return array The configuration parameters as key/value pairs.
+    */
     public function getConfig() {
       return $this->mBase->getConfig();
     }
+
+   /**
+    * Run application.
+    *
+    * Starts application execution.
+    *
+    * @return void
+    */
     public function run() {
       $this->mBase->run();
       $c = $this->getConfig();
@@ -19,6 +86,14 @@
         $this->mPeelers[ $name ]->start();
       }
     }
+
+   /**
+    * Terminate application.
+    *
+    * Stops application execution.
+    *
+    * @return void
+    */
     public function terminate() {
       return $this->mBase->terminate();
     }
