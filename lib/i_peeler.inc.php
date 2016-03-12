7c92637f8437e77ed0eaf362b1fdc63970f7a934
Espen Andersen <post@espenandersen.no>
2016-03-12 16:49:15 +0100

    Insert documentation blocks.

--- lib/i_peeler.inc.php
+++ lib/i_peeler.inc.php
@@ -1,7 +1,32 @@
 <?php
+ /**
+  * Peeler interface.
+  *
+  * Common implementation specification for all peelers.
+  *
+  * PHP version > 5.5
+  *
+  * @category   peel
+  * @package    PEEL
+  * @author     Espen Andersen <post@espenandersen.no>
+  * @copyright  2016 Espen Andersen
+  * @license    GNU General Public License, version 3
+  * @link       https://github.com/espena/peel
+  */
   interface IPeeler {
+   /**
+    * Start peeler.
+    */
     public function start();
+
+   /**
+    * Retrieve peeler data.
+    */
     public function getData( $key = '' );
+
+   /**
+    * Retrieve peeler configuration.
+    */
     public function getConfig();
   }
 ?>
