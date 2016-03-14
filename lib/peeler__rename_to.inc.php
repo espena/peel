706c14b47c7d4a2464d988ff3290d83c049ff224
Espen Andersen <post@espenandersen.no>
2016-03-14 11:52:29 +0100

    Implement template functions, i.e. 'lpad' and 'rpad'.

--- lib/peeler__rename_to.inc.php
+++ lib/peeler__rename_to.inc.php
@@ -25,10 +25,40 @@
       if( preg_match_all( '/\{([^\}]+)\}/', $pattern, $m, PREG_SET_ORDER ) ) {
         $tit = $pattern;
         foreach( $m as $tag ) {
-          $tit = str_replace( $tag[ 0 ], isset( $sourceInfo[ 'metadata' ][ $tag[ 1 ] ] ) ? $sourceInfo[ 'metadata' ][ $tag[ 1 ] ] : 'x', $tit );
+          $tit = str_replace( $tag[ 0 ], $this->expand( $sourceInfo[ 'metadata' ], $tag[ 1 ] ), $tit );
         }
       }
       return Utils::makeUniquePath( "$dir/$tit" );
+    }
+    private function expand( $metadata, $tag ) {
+      $exp = '';
+      $parts = explode( '|', $tag );
+      if( isset( $metadata[ $parts[ 0 ] ] ) ) {
+        $exp = trim( $metadata[ $parts[ 0 ] ] );
+        for( $i = 1; $i < count( $parts ); $i++ ) {
+          $params = array();
+          if( preg_match( '/^([^\(]+)\(([^\)]*)\)$/i', $parts[ $i ], $m ) ) {
+            $func = $m[ 1 ];
+            $params = explode( ',', $m[ 2 ] );
+          }
+          else {
+            $func = strtolower( trim( $parts[ $i ] ) );
+          }
+          $exp = $this->execute( $func, $params, $exp );
+        }
+      }
+      return $exp;
+    }
+    private function execute( $func, $params, $exp ) {
+      switch( $func ) {
+        case 'lpad':
+        case 'rpad':
+          if( count( $params ) >= 2 ) {
+            $exp = str_pad( $exp, $params[ 0 ], $params[ 1 ], $func == 'lpad' ? STR_PAD_LEFT : STR_PAD_RIGHT );
+          }
+          break;
+      }
+      return $exp;
     }
   }
 ?>
