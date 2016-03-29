d41253aed32d9a61afb1f421f9fba0c4f24002f7
Espen Andersen <post@espenandersen.no>
2016-03-29 22:36:03 +0200

    Add template directory path

--- www/index.php
+++ www/index.php
@@ -1,6 +1,7 @@
 <?php
   define( 'DIR_BASE', '..' );
   define( 'DIR_LIB', '../lib' );
+  define( 'DIR_TPL', '../tpl' );
   define( 'DIR_CONF', '../conf' );
   require_once( DIR_LIB . '/factory.inc.php' );
   $app = Factory::getApplication();
