643097adf500c719af0d7143866db68fa20c07ee
Espen Andersen <post@espenandersen.no>
2016-03-31 22:41:31 +0200

    Test to obtain dbxClient

--- lib/app_dropbox.inc.php
+++ lib/app_dropbox.inc.php
@@ -19,6 +19,7 @@
     }
 
     public function run() {
+      $this->handleOAuthFlow( isset( $_SERVER['PATH_INFO'] ) ? $_SERVER['PATH_INFO'] : '/' );
       $this->mBase->run();
     }
 
@@ -28,6 +29,51 @@
 
     public function tpl( $idt ) {
       $this->mBase->tpl( $idt );
+    }
+
+    private function getDbxClient() {
+      $dbxClient = null;
+      $c = $this->getConfig();
+      $accessToken      = isset( $c[ 'dropbox' ][ 'access_token' ] ) ? $c[ 'dropbox' ][ 'access_token' ] : null;
+      $clientIdentifier = isset( $c[ 'dropbox' ][ 'client_identifier' ] ) ? $c[ 'dropbox' ][ 'client_identifier' ] : null;
+      $userLocale       = isset( $c[ 'dropbox' ][ 'user_locale' ] ) ? $c[ 'dropbox' ][ 'user_locale' ] : null;
+      if( $accessToken && $clientIdentifier && $userLocale ) {
+        $dbxClient = new dbx\Client( $accessToken, $clientIdentifier, $userLocale );
+      }
+      return $dbxClient;
+    }
+
+    private function handleOAuthFlow( $req ) {
+      switch( $req ) {
+        case '/':
+          $dbxClient = $this->getDbxClient();
+          var_dump( $dbxClient );
+          die();
+          break;
+
+        case '/download':
+
+          break;
+
+        case '/upload':
+
+          break;
+
+        case '/dropbox-auth-start':
+
+          break;
+
+        case '/dropbox-auth-finish':
+
+          break;
+
+        case '/dropbox-auth-unlink':
+
+          break;
+
+        default:
+          ;
+      }
     }
 
   }
