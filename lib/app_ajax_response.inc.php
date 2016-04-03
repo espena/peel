9eab36eb7b6bf99f6e86571199356fa520d19f65
Espen Andersen <post@espenandersen.no>
2016-04-03 18:02:53 +0200

    Adding JSON response.

---
+++ lib/app_ajax_response.inc.php
@@ -0,0 +1,38 @@
+<?php
+
+  require_once( DIR_LIB . '/i_application.inc.php' );
+  
+  class AppAjaxResponse implements IWebApplication {
+
+    private $mBase;
+
+    public function __construct( $base ) {
+      $this->mBase = $base;
+    }
+
+    public function getConfig() {
+      return $this->mBase->getConfig();
+    }
+
+    public function run() {
+      header( 'content-type: application/json; charset=utf-8' );
+      $data = json_encode( array( "foo" => "bar" ) );
+      if( empty( $_GET[ 'callback' ] ) ) {
+        echo $json;
+      }
+      else {
+        echo $_GET['callback'] . '(' . $json . ')';
+      }
+    }
+
+    public function terminate() {
+      $this->mBase->terminate();
+    }
+
+    public function tpl( $idt ) {
+      $this->mBase->tpl( $idt );
+    }
+
+  }
+
+?>
