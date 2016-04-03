3b06f37967dcad2a2488e3a46eeaf26c59b37742
Espen Andersen <post@espenandersen.no>
2016-04-03 12:25:37 +0200

    Log file init.

--- www/js/peel.js
+++ www/js/peel.js
@@ -2,7 +2,8 @@
   function main() {
     $( '.peel_log' ).each( init_peel_log )
   }
-  function init_peel_log( $log ) {
+  function init_peel_log() {
+    $log = arguments[ 1 ];
     console.log( $log );
   }
   $( document ).ready( main );
