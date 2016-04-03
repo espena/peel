4ca1e6db53600b57173605177ea6c00d2528b8d7
Espen Andersen <post@espenandersen.no>
2016-04-03 12:29:21 +0200

    Revert.

--- www/js/peel.js
+++ www/js/peel.js
@@ -3,8 +3,8 @@
     $( '.peel_log' ).each( init_peel_log )
   }
   function init_peel_log() {
-    $log = arguments[ 2 ];
-    console.log( arguments );
+    $log = arguments[ 1 ];
+    console.log( $log );
   }
   $( document ).ready( main );
 } )( jQuery );
