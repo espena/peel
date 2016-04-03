f1ea04a453317a5e7a36e9fe32cf01cff49f9244
Espen Andersen <post@espenandersen.no>
2016-04-03 11:52:35 +0200

    Initialize peel_log.

--- www/js/peel.js
+++ www/js/peel.js
@@ -1,3 +1,9 @@
-( function() {
-
-} )(  );
+( function( $ ) {
+  function main() {
+    $( '.peel_log' ).each( init_peel_log )
+  }
+  function init_peel_log( $log ) {
+    console.log( $log );
+  }
+  $( document.ready( main ) );
+} )( jQuery );
