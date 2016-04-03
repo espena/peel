5994ad4a354e4a1c2aa98afd19942eed34decc65
Espen Andersen <post@espenandersen.no>
2016-04-03 10:35:02 +0200

    Adding constant DIR_TPL for template directory.

--- peel.php
+++ peel.php
@@ -1,7 +1,8 @@
 <?php
   define( 'DIR_BASE', '.' );
-  define( 'DIR_LIB', 'lib' );
-  define( 'DIR_CONF', 'conf' );
+  define( 'DIR_LIB', DIR_BASE . '/lib' );
+  define( 'DIR_TPL', DIR_BASE . '/tpl' );
+  define( 'DIR_CONF', DIR_BASE . '/conf' );
   require_once( DIR_LIB . '/factory.inc.php' );
   $app = Factory::getApplication();
   $app->run();
