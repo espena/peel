c0a06a036f91e4ccf0bfd5efbd1f89cf54ff84af
Espen Andersen <post@espenandersen.no>
2016-04-03 15:53:55 +0200

    Update interval pump.

--- www/js/peel.js
+++ www/js/peel.js
@@ -1,10 +1,36 @@
 ( function( $ ) {
+  var
+    updateStack = {
+      ts: 0,
+      ms: 1000,
+      callbacks: [ ],
+      addCallback: function( callback ) {
+        this.callbacks.push( callback );
+      }
+    };
   function main() {
-    $( '.peel_log' ).each( init_peel_log )
+    $( '.peel_log' ).each( initPeelLog );
+    startUpdateInterval();
   }
-  function init_peel_log() {
-    $log = arguments[ 1 ];
-    console.log( $log );
+  function updatePeelLog() {
+    console.log( 'updateing peel log' );
+  }
+  function initPeelLog( ts ) {
+    $log = $( arguments[ 1 ] );
+    updateStack.addCallback( updatePeelLog );
+  }
+  function startUpdateInterval() {
+    window.requestAnimationFrame(
+      function() {
+        if( updateStack.ts == 0 ) {
+          updateStack.ts = ts;
+          if( ( ts - updateStack.ts ) >= updateStack.ms ) {
+            updateStack.ts = 0;
+            for( var i = 0; i < updateStack.callbacks.length; i++ ) {
+              if( typeof( updateStack.callbacks[ i ] ) == 'Function' ) {
+                updateStack.callbacks[ i ]();
+              } } } } window.requestAnimationFrame( update );
+      } );
   }
   $( document ).ready( main );
 } )( jQuery );
