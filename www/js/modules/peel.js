0e69329636d18f18d040331e6091b89ebf4811e9
Espen Andersen <post@espenandersen.no>
2016-04-04 23:27:34 +0200

    Main JS module for peel.

---
+++ www/js/modules/peel.js
@@ -0,0 +1,72 @@
+define( [
+  'jquery',
+  'mustache',
+  'text!../tpl/log_entry.html'
+], function( $, mustache, tplLogEntry ) {
+
+  var
+    updateStack = {
+      ts: 0,
+      ms: 2000,
+      callbacks: [ ],
+      addCallback: function( callback ) {
+        this.callbacks.push( callback );
+        callback.lock = false;
+      }
+    };
+
+  function initialize() {
+    $( '.peel_log' ).each( initPeelLog );
+    updateStack.addCallback( updatePeelLog );
+    window.requestAnimationFrame( updateInterval );
+  }
+
+  function updatePeelLog() {
+    if( updatePeelLog.lock ) {
+      if( arguments[ 1 ] == 'success' ) {
+        var logData = arguments[ 0 ];
+        $( '.peel_log' ).each(
+          function( i, e ) {
+            $log = $( e );
+            $log.html( '' );
+            for( var k in logData ) {
+              $log.append( mustache.render( tplLogEntry, logData[ k ] ) );
+            }
+          }
+        );
+      }
+      updatePeelLog.lock = false;
+    }
+    else {
+      updatePeelLog.lock = true;
+      $.ajax( {
+        url: '?json=peel_log',
+        success: updatePeelLog
+      } );
+    }
+  }
+
+  function initPeelLog( ts ) {
+    $log = $( arguments[ 1 ] );
+  }
+
+  function updateInterval( ts ) {
+    if( updateStack.ts == 0 ) {
+      updateStack.ts = ts;
+    }
+    if( ( ts - updateStack.ts ) >= updateStack.ms ) {
+      updateStack.ts = 0;
+      for( var i = 0; i < updateStack.callbacks.length; i++ ) {
+        if( typeof( updateStack.callbacks[ i ] ) == 'function' ) {
+          updateStack.callbacks[ i ]();
+        }
+      }
+    }
+    window.requestAnimationFrame( updateInterval );
+  }
+
+  return {
+    'initialize': initialize
+  }
+
+} );
