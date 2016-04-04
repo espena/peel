18022bfcb3b8aeebf45972995380405b491367ae
Espen Andersen <post@espenandersen.no>
2016-04-04 07:34:07 +0200

    Add getContent() to ILogger interface.

--- lib/i_logger.inc.php
+++ lib/i_logger.inc.php
@@ -3,5 +3,6 @@
     public function message( $fmt /* ... printf-style args list */ );
     public function warning( $fmt /* ... printf-style args list */ );
     public function error( $fmt /* ... printf-style args list */ );
+    public function getContent();
   }
 ?>
