61e8f5d4097dec5a6921bff4868d0109e74ab9b9
Espen Andersen <post@espenandersen.no>
2016-04-05 23:52:56 +0200

    Implement user management tables/procedures.

--- db/create.tpl.sql
+++ db/create.tpl.sql
@@ -11,6 +11,54 @@ CREATE TABLE download (
         INDEX idx_url ( url )
     );
 
+CREATE TABLE user (
+        id_user MEDIUMINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        full_name VARCHAR( 100 ),
+        username VARCHAR( 50 ),
+        password VARCHAR( 255 ),
+        salt CHAR( 32 ),
+        created TIMESTAMP,
+        UNIQUE INDEX idx_username ( username )
+    );
+
+CREATE PROCEDURE insertUser( _full_name VARCHAR(100),
+                             _username VARCHAR(50),
+                             _password VARCHAR(255) )
+BEGIN
+    SET @salt = MD5( RAND() );
+    INSERT INTO
+        user (
+            full_name,
+            username,
+            password,
+            salt )
+    VALUES (
+        _full_name,
+        _username,
+        PASSWORD( CONCAT( _password, @salt ) ),
+        @salt );
+END;
+
+CREATE PROCEDURE deleteUser( _username VARCHAR(50) )
+BEGIN
+    DELETE FROM user WHERE username = _username;
+END;
+
+CREATE PROCEDURE verifyUser( _username VARCHAR(50),
+                             _password VARCHAR(50) )
+BEGIN
+    SELECT
+        full_name,
+        username
+    FROM
+        user
+    WHERE
+        username = _username
+    AND
+        password = PASSWORD( CONCAT( _password, salt ) )
+    LIMIT 1;
+END;
+
 CREATE PROCEDURE fetchMetadataByUrl( _url VARCHAR(255) )
 BEGIN
     SELECT * FROM download WHERE url = _url;
@@ -25,3 +73,4 @@ BEGIN
     INSERT INTO download( url, peeler, renamed, naming_status ) VALUES ( _url, _peeler, _renamed, _naming_status );
 END;
 
+CALL insertUser( 'Administrator', 'admin', '%%admin_password%%' );
