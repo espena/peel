e1e745a3df9d28e154bf3a14d43618c0e0f998c4
Espen Andersen <post@espenandersen.no>
2016-04-06 16:54:50 +0200

    Adding more stored procedures.

--- db/create.tpl.sql
+++ db/create.tpl.sql
@@ -1,13 +1,15 @@
-DROP DATABASE IF EXISTS %%database%%;
-CREATE DATABASE %%database%% DEFAULT CHARSET 'utf8';
-USE %%database%%;
+DROP DATABASE IF EXISTS %%database_name%%;
+CREATE DATABASE %%database_name%% DEFAULT CHARSET 'utf8';
+USE %%database_name%%;
 CREATE TABLE download (
         id_download BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
         modified TIMESTAMP,
         peeler VARCHAR( 255 ),
+        peeler_key VARCHAR( 100 ),
         url VARCHAR( 255 ),
         renamed VARCHAR( 255 ),
         naming_status ENUM( 'naming_ok', 'naming_collision', 'naming_incomplete' ),
+        INDEX idx_peeler ( peeler ),
         INDEX idx_url ( url )
     );
 
@@ -59,18 +61,49 @@ BEGIN
     LIMIT 1;
 END;
 
+CREATE PROCEDURE resetPeeler( _peelerToReset VARCHAR( 255 ) )
+BEGIN
+    DELETE FROM download WHERE peeler_key = _peelerToReset;
+END;
+
 CREATE PROCEDURE fetchMetadataByUrl( _url VARCHAR(255) )
 BEGIN
     SELECT * FROM download WHERE url = _url;
 END;
 
+CREATE PROCEDURE getPeelerInfoStatus( _peeler_key VARCHAR( 100 ) )
+BEGIN
+    SELECT
+        COUNT( * ) AS count_downloads,
+        MAX( modified ) AS newest_download,
+        MIN( modified ) AS oldest_download,
+        NOW() AS data_timestamp
+    FROM
+        download
+    WHERE
+        peeler_key = _peeler_key;
+END;
+
 CREATE PROCEDURE insertMetadata(
     _url VARCHAR(255),
     _peeler VARCHAR( 255 ),
+    _peeler_key VARCHAR( 100 ),
     _renamed VARCHAR( 255 ),
     _naming_status ENUM( 'naming_ok', 'naming_collision', 'naming_incomplete' ) )
 BEGIN
-    INSERT INTO download( url, peeler, renamed, naming_status ) VALUES ( _url, _peeler, _renamed, _naming_status );
+    INSERT INTO
+        download(
+            url,
+            peeler,
+            peeler_key,
+            renamed,
+            naming_status )
+    VALUES (
+        _url,
+        _peeler,
+        _peeler_key,
+        _renamed,
+        _naming_status );
 END;
 
 CALL insertUser( 'Administrator', 'admin', '%%admin_password%%' );
